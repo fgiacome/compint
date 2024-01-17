2410ac6ec6f9ff43e3d9a5a19147f64ebd155893
Francesco Giacometti <fgiacome@gmail.com>
2024-01-17 19:59:26 +0100

    quixo: formatting

--- quixo/agent.py
+++ quixo/agent.py
@@ -3,9 +3,10 @@ import torch.nn.functional as F
 import numpy as np
 from game import Player, Move, Game
 
-OUTPUT_SPACE = 4 + 16 #4*16
-INPUT_SPACE = 25 #5*5
+OUTPUT_SPACE = 4 + 16  # 4*16
+INPUT_SPACE = 25  # 5*5
 HIDDEN_NEURONS = 64
+
 
 class Policy(torch.nn.Module):
     def __init__(self):
@@ -15,13 +16,13 @@ class Policy(torch.nn.Module):
         self.fc1b = torch.nn.Linear(HIDDEN_NEURONS, HIDDEN_NEURONS)
         self.fc2 = torch.nn.Linear(HIDDEN_NEURONS, OUTPUT_SPACE)
         self.init_weights()
-    
+
     def init_weights(self):
         for m in self.modules():
             if type(m) is torch.nn.Linear:
                 torch.nn.init.xavier_normal_(m.weight)
                 torch.nn.init.zeros_(m.bias)
-    
+
     def forward(self, x):
         x = self.fc1(x)
         x_r = F.relu(x)
@@ -32,50 +33,59 @@ class Policy(torch.nn.Module):
         cell = F.softmax(x[4:], dim=0)
         return side, cell
 
+
 class NeuralPlayer(Player):
     def __init__(self, policy: Policy, train=True, print_board=False, eps=0.1):
         self.policy = policy
         self.train = train
         self.print_board = print_board
         self.action_probs = []
-        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3, weight_decay=0.001)
+        self.optimizer = torch.optim.Adam(
+            policy.parameters(), lr=1e-3, weight_decay=0.001
+        )
         self.eps = eps
 
     def map_board(self, x: int):
-        if x//4 == 0:
-            pos = (x%4, 0)
-        elif x//4 == 1:
-            pos = (4,x%4)
-        elif x//4 == 2:
-            pos = (4-x%4,4)
-        elif x//4 == 3:
-            pos = (0,4-x%4)
+        if x // 4 == 0:
+            pos = (x % 4, 0)
+        elif x // 4 == 1:
+            pos = (4, x % 4)
+        elif x // 4 == 2:
+            pos = (4 - x % 4, 4)
+        elif x // 4 == 3:
+            pos = (0, 4 - x % 4)
         return pos
 
     def get_cell_sides(self, cell):
         cell_sides = set()
-        if cell[0] == 0: cell_sides.add(2)
-        if cell[0] == 4: cell_sides.add(3)
-        if cell[1] == 0: cell_sides.add(0)
-        if cell[1] == 4: cell_sides.add(1)
+        if cell[0] == 0:
+            cell_sides.add(2)
+        if cell[0] == 4:
+            cell_sides.add(3)
+        if cell[1] == 0:
+            cell_sides.add(0)
+        if cell[1] == 4:
+            cell_sides.add(1)
         return cell_sides
-    
-    def get_allowed_action_probs(self, y_side: torch.Tensor, y_cell: torch.Tensor, game: Game):
-        y_side = y_side.reshape(4,1)
-        y_cell = y_cell.reshape(1,16)
+
+    def get_allowed_action_probs(
+        self, y_side: torch.Tensor, y_cell: torch.Tensor, game: Game
+    ):
+        y_side = y_side.reshape(4, 1)
+        y_cell = y_cell.reshape(1, 16)
         board = game.get_board()
         my_id = game.get_current_player()
         actions = y_side @ y_cell
         actions = actions + 1e-8
         for i in range(16):
             cell = self.map_board(i)
-            if board[cell[1],cell[0]] not in {-1, my_id}:
-                actions[:,i] = 0
+            if board[cell[1], cell[0]] not in {-1, my_id}:
+                actions[:, i] = 0
                 continue
             for side in self.get_cell_sides(cell):
-                actions[side,i] = 0
+                actions[side, i] = 0
         actions = actions / actions.sum()
-        return actions 
+        return actions
 
     def make_move(self, game: Game):
         board = game.get_board()
@@ -85,30 +95,29 @@ class NeuralPlayer(Player):
         # sample an action from the allowed action space
         action_probs = self.get_allowed_action_probs(y_side, y_cell, game)
         if self.train:
-            action_probs[action_probs==0] += self.eps
+            action_probs[action_probs == 0] += self.eps
             action_probs /= action_probs.sum()
         action_probs_np = action_probs.detach().numpy().ravel()
         try:
-            action = np.random.choice(64, p = action_probs_np)
+            action = np.random.choice(64, p=action_probs_np)
         except ValueError:
-            breakpoint() 
+            breakpoint()
         cell_ = action % 16
         side_ = action // 16
         cell = self.map_board(cell_)
         side = Move(side_)
         if self.train:
-            self.action_probs.append(torch.log(action_probs[side_,cell_]))
+            self.action_probs.append(torch.log(action_probs[side_, cell_]))
         if self.print_board:
             print(board)
             print(f"NeuralPlayer going for {cell}, {side}.")
         return cell, side
-    
+
     def episode_finished(self, reward):
         action_probs = torch.stack(self.action_probs, dim=0).squeeze()
         self.action_probs = []
         if self.train:
-            loss = torch.mean(action_probs*(-reward))
+            loss = torch.mean(action_probs * (-reward))
             loss.backward()
             self.optimizer.step()
             self.optimizer.zero_grad()
-        
--- quixo/train.py
+++ quixo/train.py
@@ -11,11 +11,23 @@ from copy import deepcopy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--policy", type=str, default=None, help="Policy checkpoint to use")
-parser.add_argument("--episodes", type=int, default=10_000, help="Number of training episodes")
-parser.add_argument("--save_period", type=int, default=100, help="How often to generate a new adversary")
-parser.add_argument("--checkpoint_period", type=int, default=100, help="How often to save a checkpoint")
-parser.add_argument("--load_in_folder", action="store_true", help="Wether to load all .mdl files in the current folder in the opponents pool")
-parser.add_argument("--episode_count", type=int, default=0, help="Start episode count from...")
+parser.add_argument(
+    "--episodes", type=int, default=10_000, help="Number of training episodes"
+)
+parser.add_argument(
+    "--save_period", type=int, default=100, help="How often to generate a new adversary"
+)
+parser.add_argument(
+    "--checkpoint_period", type=int, default=100, help="How often to save a checkpoint"
+)
+parser.add_argument(
+    "--load_in_folder",
+    action="store_true",
+    help="Wether to load all .mdl files in the current folder in the opponents pool",
+)
+parser.add_argument(
+    "--episode_count", type=int, default=0, help="Start episode count from..."
+)
 args = parser.parse_args()
 
 policy = Policy()
@@ -42,27 +54,30 @@ if args.load_in_folder:
 
 rewards = []
 
-pbar = tqdm(total=args.episodes-args.episode_count)
+pbar = tqdm(total=args.episodes - args.episode_count)
 while episode < args.episodes:
     game = Game()
-    game.current_player_idx = np.random.randint(0,2)
+    game.current_player_idx = np.random.randint(0, 2)
     opponent = random.choice(opponents)
     reward = game.play(player, opponent)
-    if reward == 0: reward = 1
-    elif reward == -1: reward = 0
-    else: reward = -1
+    if reward == 0:
+        reward = 1
+    elif reward == -1:
+        reward = 0
+    else:
+        reward = -1
     player.episode_finished(reward)
     rewards.append(reward)
     episode += 1
     pbar.update(1)
     if episode % args.save_period == 0:
-        new_opponent= deepcopy(player)
+        new_opponent = deepcopy(player)
         new_opponent.train = False
         opponents.append(new_opponent)
     if episode % args.checkpoint_period == 0:
         torch.save(player.policy.state_dict(), f"policy_training_{episode}.mdl")
     if episode % 100 == 0:
-        avg_reward = sum(rewards[-min(len(rewards), 50):])/min(len(rewards),50)
+        avg_reward = sum(rewards[-min(len(rewards), 50) :]) / min(len(rewards), 50)
         print(f"Avg reward after {episode} training episodes: {avg_reward:0.5f}")
 
 pbar.close()
