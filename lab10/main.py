c865a5338900bd0bad1587847878a76c287c2c45
Francesco Giacometti <fgiacome@gmail.com>
2024-02-01 16:05:53 +0100

    lab10: main.py: formatting

--- lab10/main.py
+++ lab10/main.py
@@ -1,23 +1,32 @@
 from random import sample
 
 # a board is a set of tuples, ie the coordinates of the cells
-BOARD = frozenset({
-    (0,0),(0,1),(0,2),
-    (1,0),(1,1),(1,2),
-    (2,0),(2,1),(2,2),
-        })
+BOARD = frozenset(
+    {
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (1, 0),
+        (1, 1),
+        (1, 2),
+        (2, 0),
+        (2, 1),
+        (2, 2),
+    }
+)
 
 # winning positions
 WINNING = {
-    frozenset({(0,0),(0,1),(0,2)}),
-    frozenset({(1,0),(1,1),(1,2)}),
-    frozenset({(2,0),(2,1),(2,2)}),
-    frozenset({(0,0),(1,0),(2,0)}),
-    frozenset({(0,1),(1,1),(2,1)}),
-    frozenset({(0,2),(1,2),(2,2)}),
-    frozenset({(0,0),(1,1),(2,2)}),
-    frozenset({(0,2),(1,1),(2,0)}),
-        }
+    frozenset({(0, 0), (0, 1), (0, 2)}),
+    frozenset({(1, 0), (1, 1), (1, 2)}),
+    frozenset({(2, 0), (2, 1), (2, 2)}),
+    frozenset({(0, 0), (1, 0), (2, 0)}),
+    frozenset({(0, 1), (1, 1), (2, 1)}),
+    frozenset({(0, 2), (1, 2), (2, 2)}),
+    frozenset({(0, 0), (1, 1), (2, 2)}),
+    frozenset({(0, 2), (1, 1), (2, 0)}),
+}
+
 
 # A state is a set of cells (ie the tuples in the board) for each player,
 # plus a set of free cells.
@@ -31,25 +40,28 @@ class State:
         s = ""
         for i in range(3):
             for j in range(3):
-                if (i,j) in self.x: s+='x'
-                elif (i,j) in self.o: s+='o'
-                else: s+='.'
-            s+='\n'
+                if (i, j) in self.x:
+                    s += "x"
+                elif (i, j) in self.o:
+                    s += "o"
+                else:
+                    s += "."
+            s += "\n"
         return s
-    
+
     def __eq__(self, other):
         return (self.x, self.o, self.free) == (other.x, other.o, other.free)
-    
+
     def __hash__(self):
         return hash((self.x, self.o, self.free))
-    
+
     def swap(self):
         """
         This function swaps the positions of the players
         """
         return State(self.o, self.x)
 
-    
+
 def valid(state: State):
     """
     This function checks whether a state is valid.
@@ -66,8 +78,14 @@ def valid(state: State):
             x_wins = True
         if w.issubset(state.o):
             o_wins = True
-    if x_wins and o_wins: clash = True
-    return len(state.x.intersection(state.o)) == 0 and len(state.o) - len(state.x) in {0,1} and clash == False
+    if x_wins and o_wins:
+        clash = True
+    return (
+        len(state.x.intersection(state.o)) == 0
+        and len(state.o) - len(state.x) in {0, 1}
+        and clash == False
+    )
+
 
 def win(state: State):
     """
@@ -77,19 +95,26 @@ def win(state: State):
     0 otherwise.
     """
     for w in WINNING:
-        if w.issubset(state.x): return 1
-        if w.issubset(state.o): return -1
+        if w.issubset(state.x):
+            return 1
+        if w.issubset(state.o):
+            return -1
     return 0
+
 
 def next_cell(cell):
     """
     Helper function to iterate over the cells
     """
-    if cell[1] < 2: return (cell[0], cell[1]+1)
-    elif cell[0] < 2: return (cell[0]+1, 0)
+    if cell[1] < 2:
+        return (cell[0], cell[1] + 1)
+    elif cell[0] < 2:
+        return (cell[0] + 1, 0)
     return None
 
+
 state_values = dict()
+
 
 def init_sv(state_values, state: State, cell):
     """
@@ -97,13 +122,15 @@ def init_sv(state_values, state: State, cell):
     with a call to `win`.
     """
     if cell == None:
-        if valid(state): state_values[state] = win(state)
+        if valid(state):
+            state_values[state] = win(state)
     else:
         init_sv(state_values, State(state.x.union({cell}), state.o), next_cell(cell))
         init_sv(state_values, State(state.x, state.o.union({cell})), next_cell(cell))
         init_sv(state_values, State(state.x, state.o), next_cell(cell))
 
-init_sv(state_values, State(frozenset(), frozenset()), (0,0))
+
+init_sv(state_values, State(frozenset(), frozenset()), (0, 0))
 
 # value iteration...
 delta = 1
@@ -112,7 +139,8 @@ while delta > epsilon:
     delta = 0
     for state, value in state_values.items():
         # do not update terminal states
-        if win(state) != 0 or len(state.free) == 0: continue
+        if win(state) != 0 or len(state.free) == 0:
+            continue
         successors = [State(state.x.union({cell}), state.o) for cell in state.free]
         # 2-step lookahead update
         # assuming that the other player plays randomly:
@@ -120,25 +148,34 @@ while delta > epsilon:
         next_value = max(
             [
                 # ...over the average value of the set of children...
-                sum([state_values[State(successor.x, successor.o.union({cell}))] for cell in successor.free])/len(successor.free)
-                if len(successor.free)>0 and win(successor)==0
+                sum(
+                    [
+                        state_values[State(successor.x, successor.o.union({cell}))]
+                        for cell in successor.free
+                    ]
+                )
+                / len(successor.free)
+                if len(successor.free) > 0 and win(successor) == 0
                 else win(successor)
                 # ...of every child.
                 for successor in successors
             ]
         )
         # perform the value iteration update
-        if abs(next_value - value) > delta: delta = abs(next_value - value)
+        if abs(next_value - value) > delta:
+            delta = abs(next_value - value)
         state_values[state] = next_value
+
 
 # play a game...
 def get_move(state):
     x = int(input("your move (x): "))
     y = int(input("your move (y): "))
-    while (x,y) not in state.free:
+    while (x, y) not in state.free:
         x = int(input("your move (x): "))
         y = int(input("your move (y): "))
-    return (x,y)
+    return (x, y)
+
 
 start = 0
 state = State(frozenset(), frozenset())
@@ -146,8 +183,12 @@ if start == 1:
     print(state)
     state = State(state.x, state.o.union({get_move(state)}))
 while win(state) == 0:
-    move = min(sample([move for move in state.free], k=len(state.free)), key=lambda m: state_values[State(state.x.union({m}), state.o).swap()])
+    move = min(
+        sample([move for move in state.free], k=len(state.free)),
+        key=lambda m: state_values[State(state.x.union({m}), state.o).swap()],
+    )
     state = State(state.x.union({move}), state.o)
     print(state)
-    if win(state) != 0: break
+    if win(state) != 0:
+        break
     state = State(state.x, state.o.union({get_move(state)}))
